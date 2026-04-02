ee8dc4a91731bb556da016d73fa546c9e2b78aa2
TheTwoBoom <boom@thetwoboom.xyz>
2026-04-02 12:00:47 +0200

    Add initial project structure with server implementation and configuration

---
+++ src/main/java/AsyncPlayerConfig.java
@@ -0,0 +1,6 @@
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+
+public class AsyncPlayerConfig {
+    public static void handle(AsyncPlayerConfigurationEvent event) {
+    }
+}
---
+++ src/main/java/TabList.java
@@ -0,0 +1,31 @@
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.minestom.server.event.server.ServerListPingEvent;
+import net.minestom.server.ping.Status;
+
+public class TabList {
+    public static void handle(ServerListPingEvent event) {
+        assert event.getConnection() != null;
+        var playerVersion = event.getConnection().getProtocolVersion();
+
+        var credits = Component.text("   Join to configure your forecast   ").color(TextColor.fromHexString("#555555"));
+        var description = Component.text()
+                .append(Component.text("  WeatherMC")
+                        .color(TextColor.fromHexString("#55FF55"))
+                        .decorate(TextDecoration.BOLD)
+                        .append(Component.text(" - ").color(TextColor.fromHexString("#555555")))
+                        .append(Component.text("Realtime Weather forecasts! ").color(TextColor.fromHexString("#FFFF55"))))
+                .appendNewline()
+                .append(credits)
+                .build();
+        var status = new Status(
+                description,
+                new byte[0],
+                new Status.VersionInfo("WeatherMC", playerVersion),
+                new Status.PlayerInfo(1, 404),
+                false
+        );
+        event.setStatus(status);
+    }
+}
